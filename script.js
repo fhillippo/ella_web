a6167a3b714186b2c09736915fba0c95131f3697
fhillippo <fhillippo.david20@gmail.com>
2023-05-27 14:02:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,238 @@
+var video = document.getElementById('my-video1');
+var playButton = document.getElementById('play-button1');
+
+playButton.addEventListener('click', function() {
+  if (video.paused) {
+    video.play();
+    playButton.style.display = 'none';
+  }
+});
+
+video.addEventListener('play', function() {
+  playButton.style.display = 'none';
+});
+
+video.addEventListener('pause', function() {
+  playButton.style.display = 'block';
+});
+
+// video 2
+// video 2
+var video2 = document.getElementById('my-video2');
+var playButton2 = document.getElementById('play-button2');
+
+playButton2.addEventListener('click', function() {
+  if (video2.paused) {
+    video2.play();
+    playButton2.style.display = 'none';
+  }
+});
+
+video2.addEventListener('play', function() {
+  playButton2.style.display = 'none';
+});
+
+video2.addEventListener('pause', function() {
+  playButton2.style.display = 'block';
+});
+// video 2
+// video 2
+
+
+// video 3
+// video 3
+var video3 = document.getElementById('my-video3');
+var playButton3 = document.getElementById('play-button3');
+
+playButton3.addEventListener('click', function() {
+  if (video3.paused) {
+    video3.play();
+    playButton3.style.display = 'none';
+  }
+});
+
+video3.addEventListener('play', function() {
+  playButton3.style.display = 'none';
+});
+
+video3.addEventListener('pause', function() {
+  playButton3.style.display = 'block';
+});
+// video 3
+// video 3
+
+// video 4
+// video 4
+var video4 = document.getElementById('my-video4');
+var playButton4 = document.getElementById('play-button4');
+
+playButton4.addEventListener('click', function() {
+  if (video4.paused) {
+    video4.play();
+    playButton4.style.display = 'none';
+  }
+});
+
+video4.addEventListener('play', function() {
+  playButton4.style.display = 'none';
+});
+
+video4.addEventListener('pause', function() {
+  playButton4.style.display = 'block';
+});
+// video 4
+// video 4
+
+// video 5
+// video 5
+var video5 = document.getElementById('my-video5');
+var playButton5 = document.getElementById('play-button5');
+
+playButton5.addEventListener('click', function() {
+  if (video5.paused) {
+    video5.play();
+    playButton5.style.display = 'none';
+  }
+});
+
+video5.addEventListener('play', function() {
+  playButton5.style.display = 'none';
+});
+
+video5.addEventListener('pause', function() {
+  playButton5.style.display = 'block';
+});
+// video 5
+// video 5
+
+// video 6
+// video 6
+var video6 = document.getElementById('my-video6');
+var playButton6 = document.getElementById('play-button6');
+
+playButton6.addEventListener('click', function() {
+  if (video6.paused) {
+    video6.play();
+    playButton6.style.display = 'none';
+  }
+});
+
+video6.addEventListener('play', function() {
+  playButton6.style.display = 'none';
+});
+
+video6.addEventListener('pause', function() {
+  playButton6.style.display = 'block';
+});
+// video 6
+// video 6
+
+// video 7
+// video 7
+var video7 = document.getElementById('my-video7');
+var playButton7 = document.getElementById('play-button7');
+
+playButton7.addEventListener('click', function() {
+  if (video7.paused) {
+    video7.play();
+    playButton7.style.display = 'none';
+  }
+});
+
+video7.addEventListener('play', function() {
+  playButton7.style.display = 'none';
+});
+
+video7.addEventListener('pause', function() {
+  playButton7.style.display = 'block';
+});
+// video 7
+// video 7
+
+// video 8
+// video 8
+var video8 = document.getElementById('my-video8');
+var playButton8 = document.getElementById('play-button8');
+
+playButton8.addEventListener('click', function() {
+  if (video8.paused) {
+    video8.play();
+    playButton8.style.display = 'none';
+  }
+});
+
+video8.addEventListener('play', function() {
+  playButton8.style.display = 'none';
+});
+
+video8.addEventListener('pause', function() {
+  playButton8.style.display = 'block';
+});
+// video 8
+// video 8
+
+// video 9
+// video 9
+var video9 = document.getElementById('my-video9');
+var playButton9 = document.getElementById('play-button9');
+
+playButton9.addEventListener('click', function() {
+  if (video9.paused) {
+    video9.play();
+    playButton9.style.display = 'none';
+  }
+});
+
+video9.addEventListener('play', function() {
+  playButton9.style.display = 'none';
+});
+
+video9.addEventListener('pause', function() {
+  playButton9.style.display = 'block';
+});
+// video 9
+// video 9
+
+// video 10
+// video 10
+var video10 = document.getElementById('my-video10');
+var playButton10 = document.getElementById('play-button10');
+
+playButton10.addEventListener('click', function() {
+  if (video10.paused) {
+    video10.play();
+    playButton10.style.display = 'none';
+  }
+});
+
+video10.addEventListener('play', function() {
+  playButton10.style.display = 'none';
+});
+
+video10.addEventListener('pause', function() {
+  playButton10.style.display = 'block';
+});
+// video 10
+// video 10
+
+// video 11
+// video 11
+var video11 = document.getElementById('my-video11');
+var playButton11 = document.getElementById('play-button11');
+
+playButton11.addEventListener('click', function() {
+  if (video11.paused) {
+    video11.play();
+    playButton11.style.display = 'none';
+  }
+});
+
+video11.addEventListener('play', function() {
+  playButton11.style.display = 'none';
+});
+
+video11.addEventListener('pause', function() {
+  playButton11.style.display = 'block';
+});
+// video 11
+// video 11
